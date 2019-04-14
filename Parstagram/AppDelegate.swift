455afa2165d3c2994b2aa96afb34592a18b79a21
Louise <l1jensen@ucsd.edu>
2019-04-14 12:51:38 -0700

    b

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -23,6 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://nameless-brushlands-91830.herokuapp.com/parse"
             })
         )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        }
         return true
     }
 
